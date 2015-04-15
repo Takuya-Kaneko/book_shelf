2bc7be11f7d0ac4414ae9ec5e77e14e9ffc1a39b
Takuya Kaneko <t-kaneko@nekojarashi.com>
2015-04-15 18:30:19 +0900

    update schema (migration)

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150415084610) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
